887766faf496340235d4101a302a7aa1e2ab246e
VR <vladrymarchuk@gmail.com>
2025-02-05 19:25:33 +0200

    feat: write admins for models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import (
+    Profile,
+    Post,
+    Follower,
+    Like,
+    Commentary,
+    Blocked
+)
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Follower)
+admin.site.register(Like)
+admin.site.register(Commentary)
+admin.site.register(Blocked)
