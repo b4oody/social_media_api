15c8bf25a1610167c8cb63de1c6c11e3e9e3918c
VR <vladrymarchuk@gmail.com>
2025-02-05 17:09:17 +0200

    initial project setup, add env, change db on postgresql

---
+++ social_media_api/settings/dev.py
@@ -0,0 +1,6 @@
+from social_media_api.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["171.0.0.1", "localhost"]
---
+++ social_media_api/settings/prod.py
@@ -0,0 +1,6 @@
+from social_media_api.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
