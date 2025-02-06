36dbb697a4c799c646cd50bc551829014394321d
VR <vladrymarchuk@gmail.com>
2025-02-06 20:01:53 +0200

    feat: add temporary TokenAuthentication for testing, add serializers, views

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+
+from user.views import UserCreateView, LoginUserView, ManageUserView
 
 router = DefaultRouter()
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("register/", UserCreateView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
