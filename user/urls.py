7af3f2b4c6f20af3208c931b0b14e78c943038b8
VR <vladrymarchuk@gmail.com>
2025-02-08 00:54:29 +0200

    feat: add logout

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from user.views import UserCreateView, LoginUserView, ManageUserView
+from user.views import (
+    UserCreateView,
+    LoginUserView,
+    ManageUserView,
+    LogoutView
+)
 
 router = DefaultRouter()
 
@@ -10,6 +14,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("register/", UserCreateView.as_view(), name="create"),
     path("login/", LoginUserView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
